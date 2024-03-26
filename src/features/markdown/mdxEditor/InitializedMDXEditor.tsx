151521f08ea5ba46b4e3953f49239b52fad0a25b
Simon-Honore <simon.honore6@gmail.com>
2024-03-26 12:12:08 +0100

    feat: create course and lesson

--- src/features/markdown/mdxEditor/InitializedMDXEditor.tsx
+++ src/features/markdown/mdxEditor/InitializedMDXEditor.tsx
@@ -1,47 +1,76 @@
 "use client";
 
 import {
+  BoldItalicUnderlineToggles,
+  ChangeCodeMirrorLanguage,
+  ConditionalContents,
+  DiffSourceToggleWrapper,
+  InsertCodeBlock,
   MDXEditor,
+  ShowSandpackInfo,
+  UndoRedo,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  diffSourcePlugin,
   headingsPlugin,
+  linkPlugin,
   listsPlugin,
   markdownShortcutPlugin,
   quotePlugin,
   thematicBreakPlugin,
-  type MDXEditorMethods,
-  type MDXEditorProps,
-  BoldItalicUnderlineToggles,
-  UndoRedo,
   toolbarPlugin,
+  type MDXEditorProps,
 } from "@mdxeditor/editor";
 import "@mdxeditor/editor/style.css";
-import type { ForwardedRef } from "react";
+import styles from "./mdx-editor-theme.module.css";
 
-// Only import this to the next file
-export default function InitializedMDXEditor({
-  editorRef,
-  ...props
-}: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
+export default function InitializedMDXEditor({ ...props }: MDXEditorProps) {
   return (
     <MDXEditor
+      className={styles.theme}
       contentEditableClassName="prose dark:prose-invert"
       plugins={[
         headingsPlugin(),
         listsPlugin(),
         quotePlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "js" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: { js: "JavaScript", css: "CSS", jsx: "JSX" },
+        }),
         thematicBreakPlugin(),
         markdownShortcutPlugin(),
+        diffSourcePlugin(),
+        linkPlugin(),
+
         toolbarPlugin({
           toolbarContents: () => (
-            <>
-              {" "}
-              <UndoRedo />
-              <BoldItalicUnderlineToggles />
-            </>
+            <DiffSourceToggleWrapper>
+              <ConditionalContents
+                options={[
+                  {
+                    when: (editor) => editor?.editorType === "codeblock",
+                    contents: () => <ChangeCodeMirrorLanguage />,
+                  },
+                  {
+                    when: (editor) => editor?.editorType === "sandpack",
+                    contents: () => <ShowSandpackInfo />,
+                  },
+                  {
+                    fallback: () => (
+                      <>
+                        <UndoRedo />
+                        <BoldItalicUnderlineToggles />
+                        <InsertCodeBlock />
+                      </>
+                    ),
+                  },
+                ]}
+              />
+            </DiffSourceToggleWrapper>
           ),
         }),
       ]}
       {...props}
-      ref={editorRef}
     />
   );
 }
